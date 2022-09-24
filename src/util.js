4eba13cd582f9b1dade18dfb4ca384b917b00941
DavidQF555 <54233412+DavidQF555@users.noreply.github.com>
2022-09-24 00:45:19 -0500

    Added Ephemeral Messages

--- src/util.js
+++ src/util.js
@@ -3,11 +3,13 @@ const { EmbedBuilder } = require('discord.js');
 module.exports.createSimpleFailure = message => {
 	return {
 		embeds: [new EmbedBuilder().setDescription(message).setColor('#0xFF0000')],
+		ephemeral: true,
 	};
 };
 
 module.exports.createSimpleSuccess = message => {
 	return {
 		embeds: [new EmbedBuilder().setDescription(message).setColor('#0x00FF00')],
+		ephemeral: true,
 	};
 };
